6f480ea8c483b034fd62363d460747e023a16c05
Chino <ronaldcubillo92@gmail.com>
2019-04-29 07:20:07 -0500

    Basic folder structure created

---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Kangreburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('CheeseBurger', true);
---
+++ server.js
@@ -0,0 +1,5 @@
+var express = require("express");
+
+var mysql = require("mysql");
+
+var exphbs = require("express-handlebars");
